ed7b5c72a3a7a4ae0453fb3557e366ec66f9cef1
Thibaut Monin <device.thibaut.monin@gmail.com>
2024-09-15 21:18:17 +0200

    ui(game): add custom textfield

--- lib/widgets/custom_textfield.dart
+++ lib/widgets/custom_textfield.dart
@@ -4,6 +4,8 @@ class CustomTextfield extends StatelessWidget {
   final TextEditingController controller;
   final String labelText;
   final TextCapitalization textCapitalization;
+  final TextInputType keyboardType;
+  final TextInputAction textInputAction;
   final Function(String)? onChanged;
 
   const CustomTextfield({
@@ -11,6 +13,8 @@ class CustomTextfield extends StatelessWidget {
     required this.controller,
     required this.labelText,
     this.textCapitalization = TextCapitalization.sentences,
+    this.textInputAction = TextInputAction.next,
+    this.keyboardType = TextInputType.text,
     this.onChanged,
   });
 
@@ -19,6 +23,8 @@ class CustomTextfield extends StatelessWidget {
     return TextField(
       controller: controller,
       onChanged: onChanged,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
       decoration: InputDecoration(
         labelText: labelText,
       ),
