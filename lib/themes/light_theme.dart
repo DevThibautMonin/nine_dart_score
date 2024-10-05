5a93ca0b1e527b80d6f203d4e3df32a845c237d5
Thibaut Monin <device.thibaut.monin@gmail.com>
2024-10-05 15:31:42 +0200

    ui(theme): change font (Google Fonts Nunito)

--- lib/themes/light_theme.dart
+++ lib/themes/light_theme.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:nine_dart_score/themes/custom_colors.dart';
 
 final ThemeData lightTheme = ThemeData(
   useMaterial3: true,
+  textTheme: GoogleFonts.nunitoTextTheme(),
   colorScheme: ColorScheme.fromSeed(
     seedColor: CustomColors.primaryBlue,
   ),
