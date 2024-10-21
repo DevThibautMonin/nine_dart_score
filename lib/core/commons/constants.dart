8609d2baac012ee9b76abc83514225b19b5c9c1f
Thibaut Monin <device.thibaut.monin@gmail.com>
2024-10-21 22:00:21 +0200

    core: add more constants

--- lib/core/commons/constants.dart
+++ lib/core/commons/constants.dart
@@ -3,4 +3,14 @@ class Constants {
 
   // Assets
   static const String dartTargetAsset = "assets/dart_target.svg";
+
+  // Strings
+  static const String create = "Créer";
+  static const String congratulations = "Félicitations !";
+  static const String mainMenu = "Menu principal";
+  static const String statistics = "Statistiques";
+  static const String start = "Commencer";
+  static const String players = "Joueurs";
+  static const String yes = "Oui";
+  static const String no = "Non";
 }
