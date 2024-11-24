7f5329d6ccc9877eb77277d637ff173bdc8f8db4
Thibaut Monin <device.thibaut.monin@gmail.com>
2024-11-24 19:43:52 +0100

    fix scroll behavior

--- lib/presentation/game/ui/score_selection_dialog.dart
+++ lib/presentation/game/ui/score_selection_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nine_dart_score/widgets/gaps.dart';
 
 class ScoreSelectionDialog extends StatelessWidget {
   const ScoreSelectionDialog({
@@ -12,41 +13,41 @@ class ScoreSelectionDialog extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.all(16),
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: [
             const Text(
               'Sélectionnez votre score',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 16),
-            GridView.builder(
-              shrinkWrap: true,
-              itemCount: 60,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 5,
-                mainAxisSpacing: 5,
-                crossAxisSpacing: 5,
-              ),
-              itemBuilder: (context, index) {
-                final score = index + 1;
-                return ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: const CircleBorder(),
-                    padding: const EdgeInsets.all(14),
-                  ),
-                  child: Text(
-                    '$score',
-                    style: const TextStyle(
-                      fontSize: 10,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
+            Gaps.gapH15,
+            Expanded(
+              child: GridView.builder(
+                itemCount: 60,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 5,
+                  mainAxisSpacing: 5,
+                  crossAxisSpacing: 5,
+                ),
+                itemBuilder: (context, index) {
+                  final score = index + 1;
+                  return ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(14),
                     ),
-                  ),
-                  onPressed: () {
-                    Navigator.pop(context, score);
-                  },
-                );
-              },
+                    child: Text(
+                      '$score',
+                      style: const TextStyle(
+                        fontSize: 10,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context, score);
+                    },
+                  );
+                },
+              ),
             ),
           ],
         ),
