eab2c8a61508c51ef3612eb114629fd96794a904
Thibaut Monin <device.thibaut.monin@gmail.com>
2024-12-17 21:03:36 +0100

    bump 1.0.1
    Add firebase crashlytics
    Add firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,32 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:nine_dart_score/core/commons/constants.dart';
 import 'package:nine_dart_score/core/di/get_it_setup.dart';
+import 'package:nine_dart_score/firebase_options.dart';
 import 'package:nine_dart_score/presentation/tabs_screen/tabs_screen.dart';
 import 'package:nine_dart_score/themes/dark_theme.dart';
 import 'package:nine_dart_score/themes/light_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   setupGetIt();
   runApp(const MyApp());
 }
